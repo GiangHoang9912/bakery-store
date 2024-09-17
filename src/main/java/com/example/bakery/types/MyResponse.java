cf91a72fc1baf8d559073144c8a9843fe36b7361
thuylinh <thuylinh1610@gmail.com>
2024-09-17 16:02:20 +0700

    init project

---
+++ src/main/java/com/example/bakery/types/MyResponse.java
@@ -0,0 +1,19 @@
+package com.example.bakery.types; 
+
+public class MyResponse {
+    private String message;
+
+    public MyResponse(String message) {
+        this.message = message;
+    }
+
+    // Getter
+    public String getMessage() {
+        return message;
+    }
+
+    // Setter
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
